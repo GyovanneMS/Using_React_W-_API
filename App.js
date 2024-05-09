d292a8634f3299abd0c0100ec8c5241b91258725
GyovanneMS <gyovannecontato@gmail.com>
2024-05-09 15:33:54 -0300

    New project

--- App.js
+++ App.js
@@ -1,44 +1,29 @@
-import { LinkingContext } from "@react-navigation/native";
 import React, {useState, useEffect} from "react";
 import { SafeAreaView, View, Text, StyleSheet, StatusBar } from "react-native";
+import axios from "axios";
 
 export default function App() {
 
-  const [moedas, definirMoedas] = useState();
+  const [norris, definirNorris] = useState();
 
   useEffect(function() {
-    const ObterMoedas = async function() {
-      const url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json";
+    const ObterConselho = async function() {
+      const url = "https://api.chucknorris.io/jokes/random";
       const resultado = await axios.get(url);
       const dados = resultado.data;
-      definirMoedas(dados.used);
+      console.log(resultado);
+      definirNorris(dados.value);
     }
-    ObterMoedas();
+    ObterConselho();
   }, [])
 
   return <SafeAreaView style={Style.tela}>
-    <StatusBar barStyle={LinkingContext} />
+    <StatusBar barStyle={"light-content"} />
 
-    <Text style={Style.titulo}>💵💵💴CONVERTER💶💷💸🤑</Text>
+    <Text style={Style.titulo}>💵💵💴Chuck Norris💶💷💸🤑</Text>
     <View style={Style.tabela}>
-      <Text style={Style.tabelaTitulo}>Dólar</Text>
-      <Text> USD 1,00</Text>
-    </View>
-    <View style={Style.tabela}>
-      <Text style={ Style.tabelaTitulo}>Reais</Text>
-      <Text>BRL { moedas ? moedas.br: 0}</Text>
-    </View>
-    <View style={Style.tabela}>
-      <Text style={ Style.tabelaTitulo}>Bitcoin</Text>
-      <Text>BTC { moedas ? moedas.btc: 0}</Text>
-    </View>
-    <View style={Style.tabela}>
-      <Text style={ Style.tabelaTitulo}>Euro</Text>
-      <Text>EUR { moedas ? moedas.beurr: 0}</Text>
-    </View>
-    <View style={Style.tabela}>
-      <Text style={ Style.tabelaTitulo}>Libra Esterlina</Text>
-      <Text>GBP { moedas ? moedas.gbp: 0}</Text>
+      <Text style={Style.tabelaTitulo}>Conselho do Chuck Norris</Text>
+      <Text> {norris} </Text>
     </View>
   </SafeAreaView>
 };
