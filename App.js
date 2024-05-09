dcd1c62bd08b81bedb0c74a81aaf98f1d18d34fd
GyovanneMS <gyovannecontato@gmail.com>
2024-05-09 15:08:10 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { LinkingContext } from "@react-navigation/native";
+import React, {useState, useEffect} from "react";
+import { SafeAreaView, View, Text, StyleSheet, StatusBar } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
+  const [moedas, definirMoedas] = useState();
+
+  useEffect(function() {
+    const ObterMoedas = async function() {
+      const url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json";
+      const resultado = await axios.get(url);
+      const dados = resultado.data;
+      definirMoedas(dados.used);
+    }
+    ObterMoedas();
+  }, [])
+
+  return <SafeAreaView style={Style.tela}>
+    <StatusBar barStyle={LinkingContext} />
+
+    <Text style={Style.titulo}>💵💵💴CONVERTER💶💷💸🤑</Text>
+    <View style={Style.tabela}>
+      <Text style={Style.tabelaTitulo}>Dólar</Text>
+      <Text> USD 1,00</Text>
+    </View>
+    <View style={Style.tabela}>
+      <Text style={ Style.tabelaTitulo}>Reais</Text>
+      <Text>BRL { moedas ? moedas.br: 0}</Text>
+    </View>
+    <View style={Style.tabela}>
+      <Text style={ Style.tabelaTitulo}>Bitcoin</Text>
+      <Text>BTC { moedas ? moedas.btc: 0}</Text>
+    </View>
+    <View style={Style.tabela}>
+      <Text style={ Style.tabelaTitulo}>Euro</Text>
+      <Text>EUR { moedas ? moedas.beurr: 0}</Text>
+    </View>
+    <View style={Style.tabela}>
+      <Text style={ Style.tabelaTitulo}>Libra Esterlina</Text>
+      <Text>GBP { moedas ? moedas.gbp: 0}</Text>
+    </View>
+  </SafeAreaView>
+};
+
+const Style = StyleSheet.create({
+  tela: {
+    backgroundColor: "#A3C9AA",
+    alignItems: "center",
+    justifyContent: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  titulo: {
+    color: "#12372A",
+    fontSize: 32,
+    fontWeight: "bold",
+    paddingVertical: 16,
+  },
+  tabela: {
+    backgroundColor: "#FFF3CF",
+    borderRadius: 16,
+    margin: 8,
+    padding: 16,
+  },
+  tabelaTitulo: {
+    fontSize: 20,
+  }
 });
